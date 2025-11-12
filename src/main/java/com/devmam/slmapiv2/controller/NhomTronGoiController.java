b7c8e6f25aefe0bb0340fc7ff28d3faff5b55273
minhminh03 <125256910+HaiMinhDoan@users.noreply.github.com>
2025-11-12 13:41:55 +0700

    push

---
+++ src/main/java/com/devmam/slmapiv2/controller/NhomTronGoiController.java
@@ -0,0 +1,52 @@
+package com.devmam.slmapiv2.controller;
+
+import com.devmam.slmapiv2.dto.request.BaseFilterRequest;
+import com.devmam.slmapiv2.dto.request.entities.NhomTronGoiCreatingDto;
+import com.devmam.slmapiv2.dto.response.ResponseData;
+import com.devmam.slmapiv2.dto.response.entities.NhomTronGoiDto;
+import com.devmam.slmapiv2.mapper.NhomTronGoiMapper;
+import com.devmam.slmapiv2.services.impl.enities.NhomTronGoiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/basic-api/nhom-tron-goi")
+public class NhomTronGoiController {
+
+    @Autowired
+    private NhomTronGoiService nhomTronGoiService;
+
+    @Autowired
+    private NhomTronGoiMapper nhomTronGoiMapper;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseData<NhomTronGoiDto>> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(
+                ResponseData.<NhomTronGoiDto>builder()
+                        .status(200)
+                        .error(null)
+                        .message("Success")
+                        .data(nhomTronGoiMapper.toDto(nhomTronGoiService.getOne(id).orElse(null)))
+                        .build()
+        );
+    }
+
+    @PostMapping("/filter")
+    public ResponseEntity<ResponseData<Page<NhomTronGoiDto>>> filter(@RequestBody BaseFilterRequest filter) {
+        return ResponseEntity.ok(
+                ResponseData.<Page<NhomTronGoiDto>>builder()
+                        .status(200)
+                        .error(null)
+                        .message("Success")
+                        .data(nhomTronGoiMapper.toDtoPage(nhomTronGoiService.filter(filter)))
+                        .build()
+        );
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseData<NhomTronGoiDto>> create(@RequestBody NhomTronGoiCreatingDto dto) {
+        return nhomTronGoiService.create(dto);
+    }
+}
